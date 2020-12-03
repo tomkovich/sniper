556552ce689f65b9dcffab63c70fff179f9ad6f4
Yana Tamkovich <yana.tamkovich@startmatter.com>
2020-12-03 15:00:05 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 1488
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT} port`)
