6baa6fe2f71787ec8cdce521c3fe77faa7264468
ya_tomkovich <yana.tamkovich@startmatter.com>
2020-12-04 21:59:32 +0300

    Add authentication with Google OAuth

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: "1003186207877-0kj8jbgdu35d3uqvp4h0vmr1fevmahq2.apps.googleusercontent.com",
+    googleClientSecret: "CN3RoNtzXT1I3Q0D5o6TuS0i"
+}
--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const express = require('express')
+const express = require('express');
+const { googleClientID, googleClientSecret } = require('./config/keys');
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send({
-        hi: "there"
-    })
-})
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+ 
+passport.use(new GoogleStrategy({
+    clientID: googleClientID,
+    clientSecret: googleClientSecret,
+    callbackURL: "/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile)
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login', successRedirect: '/' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 const PORT = process.env.PORT || 5000
-
 app.listen(PORT, () => {
     console.log(`Server is running at ${PORT} port`)
 })
