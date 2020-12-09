e02e430bcdb13e52d33db1b378e9901f0b25cc3e
Yana Tamkovich <yana.tamkovich@startmatter.com>
2020-12-09 13:09:59 +0300

    Add @material-ui

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,26 +3,110 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/target.svg";
+import { COLORS } from "../../styles/colors";
 import Account from "./Account";
 
+// Material UI Components
+import { Link as LinkGoogle } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import { MENU_ITEMS } from "../../constans/menuItems";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginRight: drawerWidth,
+    backgroundColor: COLORS.MAIN,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+  },
+  logo: {
+    width: 50,
+    maxHeight: "100%",
+    marginRight: 20,
+  },
+  googleLink: {
+    textDecoration: "none",
+    fontSize: 13,
+    "&:hover": {
+      textDecoration: "none",
+      color: "#e3e3e3",
+    },
+  },
+}));
+
 const Header = ({ user }) => {
+  const classes = useStyles();
+
   const renderMarkup = user ? (
     <Account user={user} />
   ) : (
-    <a href="/auth/google">Login with Google</a>
+    <LinkGoogle
+      href="/auth/google"
+      color="inherit"
+      className={classes.googleLink}
+    >
+      Login with Google
+    </LinkGoogle>
   );
 
   return (
-    <nav>
-      <div className="nav-wrapper deep-purple darken-1">
-        <Link to={user ? "/posts" : "/"} className="brand-logo ml15">
-          <img src={logo} />
-        </Link>
-        <ul className="right hide-on-med-and-down">
-          <li className="payments-component">{renderMarkup}</li>
-        </ul>
-      </div>
-    </nav>
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <div className={classes.logo}>
+            <img src={logo} />
+          </div>
+          <Typography variant="h6" noWrap>
+            {renderMarkup}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="right"
+      >
+        <div className={classes.toolbar} />
+        <List component="div">
+          {MENU_ITEMS.map(({ title, icon }) => (
+            <ListItem key={title} component={title === "logout" ? "a" : Link}>
+              <ListItemIcon>{icon}</ListItemIcon>
+              <ListItemText primary={title} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
   );
 };
 
---
+++ client/src/constans/menuItems.js
@@ -0,0 +1,8 @@
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+export const MENU_ITEMS = [
+  {
+    title: "Logout",
+    icon: <ExitToAppIcon />,
+  },
+];
